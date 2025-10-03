f088c29418afd9b26a4f7cc7bb866b01d06c1368
Juan Torres <juan93git@gmail.com>
2025-10-03 18:50:51 +0200

    CreateExercise use case

---
+++ src/use-cases/exercise/CreateExercise/CreateExercise.usecase.ts
@@ -0,0 +1,24 @@
+import { ExercisesRepo } from '@/domain/repos/ExercisesRepo.port';
+import { Exercise } from '@/domain/entities/exercise/Exercise';
+import { v4 as uuidv4 } from 'uuid';
+
+export type CreateExerciseUsecaseRequest = {
+  name: string;
+};
+
+export class CreateExerciseUsecase {
+  constructor(private exercisesRepo: ExercisesRepo) {}
+
+  async execute(request: CreateExerciseUsecaseRequest): Promise<Exercise> {
+    const newExercise = Exercise.create({
+      id: uuidv4(),
+      name: request.name,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await this.exercisesRepo.saveExercise(newExercise);
+
+    return newExercise;
+  }
+}
---
+++ src/use-cases/exercise/CreateExercise/__tests__/CreateExercise.usecase.test.ts
@@ -0,0 +1,45 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { CreateExerciseUsecase } from '../CreateExercise.usecase';
+import { MemoryExercisesRepo } from '@/infra/memory/MemoryExercisesRepo';
+import { ValidationError } from '@/domain/common/errors';
+
+describe('CreateExerciseUsecase', () => {
+  let exercisesRepo: MemoryExercisesRepo;
+  let createExerciseUsecase: CreateExerciseUsecase;
+
+  beforeEach(() => {
+    exercisesRepo = new MemoryExercisesRepo();
+    createExerciseUsecase = new CreateExerciseUsecase(exercisesRepo);
+  });
+
+  it('should create and save a new exercise', async () => {
+    const request = { name: 'Push Up' };
+
+    const exercise = await createExerciseUsecase.execute(request);
+
+    expect(exercise).toHaveProperty('id');
+    expect(exercise.name).toBe(request.name);
+    expect(exercise).toHaveProperty('createdAt');
+    expect(exercise).toHaveProperty('updatedAt');
+
+    const savedExercise = await exercisesRepo.getExerciseById(exercise.id);
+    expect(savedExercise).toEqual(exercise);
+  });
+
+  it('should throw an error if name is empty', async () => {
+    const request = { name: '' };
+
+    await expect(createExerciseUsecase.execute(request)).rejects.toThrow(
+      ValidationError
+    );
+  });
+
+  it('should throw an error if name is not a string', async () => {
+    const request = { name: 123 };
+
+    // @ts-expect-error testing invalid input
+    await expect(createExerciseUsecase.execute(request)).rejects.toThrow(
+      ValidationError
+    );
+  });
+});
