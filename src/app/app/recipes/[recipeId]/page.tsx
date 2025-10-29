bc825e7863a8f118c439087960c408f12e829dce
Juan Torres <JuanTorresNavarro93@gmail.com>
2025-10-29 22:17:10 +0100

    Get recipe by id from repo and show basic recipe info.

--- src/app/app/recipes/[recipeId]/page.tsx
+++ src/app/app/recipes/[recipeId]/page.tsx
@@ -1,13 +1,23 @@
-'use client';
+import { AppGetRecipeByIdForUserUsecase } from '@/interface-adapters/app/use-cases/recipe';
 import { RecipeDTO } from '@/application-layer/dtos/RecipeDTO';
-import { useParams } from 'next/navigation';
 
-export default function RecipePage() {
-  const params = useParams();
-  const { recipeId } = params;
+export default async function RecipePage({
+  params,
+}: {
+  params: Promise<{ recipeId: string }>;
+}) {
+  const { recipeId } = await params;
+  const recipe: RecipeDTO | null = await AppGetRecipeByIdForUserUsecase.execute(
+    {
+      id: recipeId,
+      userId: 'dev-user', // TODO IMPORTANT: Replace with actual user ID from authenticated user
+    }
+  );
 
-  // TODO fetch recipe by id from the server
-  const recipe: RecipeDTO | null = null;
-
-  return <div className="p-6">Recipe Id: {recipeId}</div>;
+  if (!recipe) return <div>No recipe found</div>;
+  return (
+    <div className="p-6">
+      <h2>{recipe.name}</h2>
+    </div>
+  );
 }
