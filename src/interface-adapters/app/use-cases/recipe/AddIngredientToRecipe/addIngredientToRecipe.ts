5d810dcbe3043858611866afb942d3753b2be73c
Juan Torres <JuanTorresNavarro93@gmail.com>
2025-11-14 13:11:04 +0100

    Missing file in a previous commit

--- src/interface-adapters/app/use-cases/recipe/AddIngredientToRecipe/addIngredientToRecipe.ts
+++ src/interface-adapters/app/use-cases/recipe/AddIngredientToRecipe/addIngredientToRecipe.ts
@@ -1,6 +1,8 @@
 import { AppRecipesRepo } from '@/interface-adapters/app/repos/AppRecipesRepo';
+import { AppIngredientLinesRepo } from '@/interface-adapters/app/repos/AppIngredientLinesRepo';
 import { AddIngredientToRecipeUsecase } from '@/application-layer/use-cases/recipe/AddIngredientToRecipe/AddIngredientToRecipe.usecase';
 
 export const AppAddIngredientToRecipeUsecase = new AddIngredientToRecipeUsecase(
-  AppRecipesRepo
+  AppRecipesRepo,
+  AppIngredientLinesRepo
 );
