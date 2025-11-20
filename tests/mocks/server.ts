3f7c24a725bfbeb21b9d83f7623fa911a1fd9a2a
Juan Torres <JuanTorresNavarro93@gmail.com>
2025-11-20 09:27:34 +0100

    Correctly type createServer function for API mocks

--- tests/mocks/server.ts
+++ tests/mocks/server.ts
@@ -1,17 +1,24 @@
 import { setupServer } from 'msw/node';
 
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse, PathParams } from 'msw';
 
 type HandlerConfig = {
   path: string;
   method: 'get' | 'post' | 'put' | 'delete';
-  response: any;
+  response: (info: {
+    request: Request;
+    params: PathParams;
+    cookies: Record<string, string>;
+  }) => unknown;
 };
 
 export function createServer(handlersConfig: HandlerConfig[]) {
   const handlers = handlersConfig.map((config) => {
-    return http[config.method || 'get'](config.path, (req, res, ctx) => {
-      return HttpResponse.json(config.response(req, res, ctx));
+    return http[config.method || 'get'](config.path, (info) => {
+      // DOC: info.params: string | string[] | undefined
+      return HttpResponse.json(
+        config.response(info) as Record<string, unknown>
+      );
     });
   });
 
