22d941a095a1644287766b5692a21a77f44deef7
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-01-07 19:07:54 +0000

    Tested addition of ingredient to existing recipe in GUI

--- src/app/app/recipes/[recipeId]/__tests__/recipePage.test.tsx
+++ src/app/app/recipes/[recipeId]/__tests__/recipePage.test.tsx
@@ -39,11 +39,15 @@ async function setup() {
   const { mockRecipes: recipes } = await createMockRecipes();
   mockRecipes = recipes;
 
+  const renderedRecipe = recipes[0];
+
   const ui = await RecipePage({
     // IMPORTANT DOC: params is a Promise due to Next.js implementation
-    params: Promise.resolve({ recipeId: recipes[0].id }),
+    params: Promise.resolve({ recipeId: renderedRecipe.id }),
   });
   render(ui);
+
+  return { renderedRecipe };
 }
 
 afterEach(() => {
@@ -52,211 +56,255 @@ afterEach(() => {
 });
 
 describe('RecipePage', () => {
-  it('renders recipe title', async () => {
-    await setup();
+  describe('Shown info', () => {
+    it('renders recipe title', async () => {
+      await setup();
 
-    const title = await screen.findByDisplayValue(mockRecipes[0].name);
+      const title = await screen.findByDisplayValue(mockRecipes[0].name);
 
-    expect(title).toBeInTheDocument();
-  });
+      expect(title).toBeInTheDocument();
+    });
 
-  it('renders rounded calories', async () => {
-    await setup();
+    it('renders rounded calories', async () => {
+      await setup();
 
-    const calories = mockRecipes[0].calories;
-    const roundedCalories = Math.round(calories).toString();
-
-    const caloriesElement = screen.getByText(roundedCalories);
-
-    expect(calories).toBeDefined();
-    expect(calories).toBeGreaterThan(0);
-    expect(caloriesElement).toBeInTheDocument();
-  });
-
-  it('renders rounded protein', async () => {
-    await setup();
-
-    const protein = mockRecipes[0].protein;
-    const roundedProtein = Math.round(protein).toString();
-
-    const proteinElement = screen.getByText(roundedProtein);
-
-    expect(protein).toBeDefined();
-    expect(protein).toBeGreaterThan(0);
-    expect(proteinElement).toBeInTheDocument();
-  });
-
-  it('renders ingredient lines', async () => {
-    await setup();
-
-    const ingredientLines = mockRecipes[0].ingredientLines;
-
-    for (const line of ingredientLines) {
-      const ingredientLineElement = screen.getByText(line.ingredient.name);
-      expect(ingredientLineElement).toBeInTheDocument();
-    }
-  });
-
-  it('renders ingredient quantities', async () => {
-    await setup();
-
-    const ingredientLines = mockRecipes[0].ingredientLines;
-
-    for (const line of ingredientLines) {
-      const quantityText = `${line.quantityInGrams}`;
-      const quantityElement = screen.getByDisplayValue(quantityText);
-      expect(quantityElement).toBeInTheDocument();
-    }
-  });
-
-  it('renders total rounded calories per ingredient', async () => {
-    await setup();
-
-    const ingredientLines = mockRecipes[0].ingredientLines;
-
-    for (const line of ingredientLines) {
-      const roundedCalories = Math.round(line.calories).toString();
+      const calories = mockRecipes[0].calories;
+      const roundedCalories = Math.round(calories).toString();
 
       const caloriesElement = screen.getByText(roundedCalories);
+
+      expect(calories).toBeDefined();
+      expect(calories).toBeGreaterThan(0);
       expect(caloriesElement).toBeInTheDocument();
-    }
-  });
+    });
 
-  it('renders total rounded protein per ingredient', async () => {
-    await setup();
+    it('renders rounded protein', async () => {
+      await setup();
 
-    const ingredientLines = mockRecipes[0].ingredientLines;
-
-    for (const line of ingredientLines) {
-      const roundedProtein = Math.round(line.protein).toString();
+      const protein = mockRecipes[0].protein;
+      const roundedProtein = Math.round(protein).toString();
 
       const proteinElement = screen.getByText(roundedProtein);
+
+      expect(protein).toBeDefined();
+      expect(protein).toBeGreaterThan(0);
       expect(proteinElement).toBeInTheDocument();
-    }
+    });
+
+    it('renders ingredient lines', async () => {
+      await setup();
+
+      const ingredientLines = mockRecipes[0].ingredientLines;
+
+      for (const line of ingredientLines) {
+        const ingredientLineElement = screen.getByText(line.ingredient.name);
+        expect(ingredientLineElement).toBeInTheDocument();
+      }
+    });
+
+    it('renders ingredient quantities', async () => {
+      await setup();
+
+      const ingredientLines = mockRecipes[0].ingredientLines;
+
+      for (const line of ingredientLines) {
+        const quantityText = `${line.quantityInGrams}`;
+        const quantityElement = screen.getByDisplayValue(quantityText);
+        expect(quantityElement).toBeInTheDocument();
+      }
+    });
+
+    it('renders total rounded calories per ingredient', async () => {
+      await setup();
+
+      const ingredientLines = mockRecipes[0].ingredientLines;
+
+      for (const line of ingredientLines) {
+        const roundedCalories = Math.round(line.calories).toString();
+
+        const caloriesElement = screen.getByText(roundedCalories);
+        expect(caloriesElement).toBeInTheDocument();
+      }
+    });
+
+    it('renders total rounded protein per ingredient', async () => {
+      await setup();
+
+      const ingredientLines = mockRecipes[0].ingredientLines;
+
+      for (const line of ingredientLines) {
+        const roundedProtein = Math.round(line.protein).toString();
+
+        const proteinElement = screen.getByText(roundedProtein);
+        expect(proteinElement).toBeInTheDocument();
+      }
+    });
   });
 
-  it('allows updating ingredient quantity', async () => {
-    await setup();
+  describe('Behavior', () => {
+    it('allows updating ingredient quantity', async () => {
+      await setup();
 
-    const recipe = mockRecipes[0];
+      const recipe = mockRecipes[0];
 
-    const ingredientLines = recipe.ingredientLines;
-    const lineToUpdate = ingredientLines[0];
+      const ingredientLines = recipe.ingredientLines;
+      const lineToUpdate = ingredientLines[0];
 
-    const newQuantity = '500';
+      const newQuantity = '500';
 
-    const quantityInput = screen.getByDisplayValue(
-      lineToUpdate.quantityInGrams.toString()
-    );
+      const quantityInput = screen.getByDisplayValue(
+        lineToUpdate.quantityInGrams.toString()
+      );
 
-    await userEvent.clear(quantityInput);
-    await userEvent.type(quantityInput, newQuantity);
+      await userEvent.clear(quantityInput);
+      await userEvent.type(quantityInput, newQuantity);
 
-    expect(quantityInput).toHaveValue(500);
+      expect(quantityInput).toHaveValue(500);
 
-    await waitFor(async () => {
-      const updatedIngredientLine = await recipesRepo
-        .getRecipeById(recipe.id)
-        .then((r) =>
-          r?.ingredientLines.find(
-            (line) => line.ingredient.id === lineToUpdate.ingredient.id
-          )
+      await waitFor(async () => {
+        const updatedIngredientLine = await recipesRepo
+          .getRecipeById(recipe.id)
+          .then((r) =>
+            r?.ingredientLines.find(
+              (line) => line.ingredient.id === lineToUpdate.ingredient.id
+            )
+          );
+
+        expect(updatedIngredientLine?.quantityInGrams).toBe(
+          parseInt(newQuantity)
         );
+      });
+    });
 
-      expect(updatedIngredientLine?.quantityInGrams).toBe(
-        parseInt(newQuantity)
+    it('duplicates recipe', async () => {
+      await setup();
+
+      const initialRecipes = recipesRepo.countForTesting();
+
+      const duplicateButton = screen.getByTestId('duplicate-recipe-button');
+
+      await userEvent.click(duplicateButton);
+
+      await waitFor(() => {
+        const allRecipes = recipesRepo.countForTesting();
+        expect(allRecipes).toBe(initialRecipes + 1);
+      });
+    });
+
+    it('deletes recipe', async () => {
+      await setup();
+
+      const initialRecipes = recipesRepo.countForTesting();
+
+      const deleteButton = screen.getByTestId('delete-recipe-button');
+
+      await userEvent.click(deleteButton);
+
+      await waitFor(() => {
+        const allRecipes = recipesRepo.countForTesting();
+        expect(allRecipes).toBe(initialRecipes - 1);
+      });
+    });
+
+    it('can remove ingredients from recipe', async () => {
+      await setup();
+
+      const recipe = mockRecipes[0];
+
+      const initialIngredientLinesCount = recipe.ingredientLines.length;
+
+      const ingredientLinesContainer = screen.getByTestId(
+        'ingredient-lines-container'
       );
+
+      const ingredientLineElement = ingredientLinesContainer
+        .children[0] as HTMLElement;
+      const removeButton = within(ingredientLineElement).getByTestId(
+        'nutritional-summary-delete-button'
+      );
+
+      await userEvent.click(removeButton);
+
+      await waitFor(async () => {
+        const updatedRecipe = await recipesRepo.getRecipeById(recipe.id);
+        expect(updatedRecipe).not.toBeNull();
+        expect(updatedRecipe!.ingredientLines.length).toBe(
+          initialIngredientLinesCount - 1
+        );
+      });
     });
-  });
 
-  it('duplicates recipe', async () => {
-    await setup();
+    it('cannot leave a recipe with less than one ingredient', async () => {
+      await setup();
 
-    const initialRecipes = recipesRepo.countForTesting();
+      const recipe = mockRecipes[0];
 
-    const duplicateButton = screen.getByTestId('duplicate-recipe-button');
+      // Remove all ingredients except one
+      const deleteIngredientButtons = await screen.findAllByTestId(
+        'nutritional-summary-delete-button'
+      );
 
-    await userEvent.click(duplicateButton);
+      const [lastButton, ...restButtons] = deleteIngredientButtons;
 
-    await waitFor(() => {
-      const allRecipes = recipesRepo.countForTesting();
-      expect(allRecipes).toBe(initialRecipes + 1);
-    });
-  });
+      for (const button of restButtons) {
+        await userEvent.click(button);
+      }
 
-  it('deletes recipe', async () => {
-    await setup();
-
-    const initialRecipes = recipesRepo.countForTesting();
-
-    const deleteButton = screen.getByTestId('delete-recipe-button');
-
-    await userEvent.click(deleteButton);
-
-    await waitFor(() => {
-      const allRecipes = recipesRepo.countForTesting();
-      expect(allRecipes).toBe(initialRecipes - 1);
-    });
-  });
-
-  it('can remove ingredients from recipe', async () => {
-    await setup();
-
-    const recipe = mockRecipes[0];
-
-    const initialIngredientLinesCount = recipe.ingredientLines.length;
-
-    const ingredientLinesContainer = screen.getByTestId(
-      'ingredient-lines-container'
-    );
-
-    const ingredientLineElement = ingredientLinesContainer
-      .children[0] as HTMLElement;
-    const removeButton = within(ingredientLineElement).getByTestId(
-      'nutritional-summary-delete-button'
-    );
-
-    await userEvent.click(removeButton);
-
-    await waitFor(async () => {
       const updatedRecipe = await recipesRepo.getRecipeById(recipe.id);
+
       expect(updatedRecipe).not.toBeNull();
-      expect(updatedRecipe!.ingredientLines.length).toBe(
-        initialIngredientLinesCount - 1
-      );
+      expect(updatedRecipe!.ingredientLines.length).toBe(1);
+
+      await userEvent.click(lastButton);
+
+      const finalRecipe = await recipesRepo.getRecipeById(recipe.id);
+
+      expect(finalRecipe).not.toBeNull();
+      expect(finalRecipe!.ingredientLines.length).toBe(1);
     });
+
+    it('can add new ingredient to recipe', async () => {
+      const { renderedRecipe } = await setup();
+
+      const existingLinesContainer = screen.getByTestId(
+        'ingredient-lines-container'
+      );
+      const initialIngredientLinesCount =
+        existingLinesContainer.childElementCount;
+
+      const searchIngredientInput = screen.getByPlaceholderText(
+        'Buscar ingredientes...'
+      );
+
+      const searchButton = screen.getByRole('button', { name: 'Buscar' });
+
+      await userEvent.type(searchIngredientInput, 'celery');
+      await userEvent.click(searchButton);
+
+      const ingredientList = await screen.findByTestId('ingredient-list');
+
+      await waitFor(() => {
+        expect(ingredientList.childNodes.length).toBeGreaterThan(0);
+      });
+
+      const searchResultItem = await screen.findByText(/celery/i);
+      await userEvent.click(searchResultItem);
+
+      const addButton = screen.getByRole('button', { name: /añadir/i });
+      await userEvent.click(addButton);
+
+      await waitFor(() => {
+        // Get recipe from repo to check ingredient lines count (tested in this way because NextJS revalidation does not work in test environment)
+        recipesRepo.getRecipeById(renderedRecipe.id).then((updatedRecipe) => {
+          expect(updatedRecipe).not.toBeNull();
+          expect(updatedRecipe!.ingredientLines.length).toBe(
+            initialIngredientLinesCount + 1
+          );
+        });
+      });
+    });
+
+    // TODO NEXT test update recipe name
+    // TODO NEXT test and implement update recipe image
   });
-
-  it('cannot leave a recipe with less than one ingredient', async () => {
-    await setup();
-
-    const recipe = mockRecipes[0];
-
-    // Remove all ingredients except one
-    const deleteIngredientButtons = await screen.findAllByTestId(
-      'nutritional-summary-delete-button'
-    );
-
-    const [lastButton, ...restButtons] = deleteIngredientButtons;
-
-    for (const button of restButtons) {
-      await userEvent.click(button);
-    }
-
-    const updatedRecipe = await recipesRepo.getRecipeById(recipe.id);
-
-    expect(updatedRecipe).not.toBeNull();
-    expect(updatedRecipe!.ingredientLines.length).toBe(1);
-
-    await userEvent.click(lastButton);
-
-    const finalRecipe = await recipesRepo.getRecipeById(recipe.id);
-
-    expect(finalRecipe).not.toBeNull();
-    expect(finalRecipe!.ingredientLines.length).toBe(1);
-  });
-
-  // TODO test update recipe name
-  // TODO test update recipe image
-  // TODO test Add ingredient
 });
