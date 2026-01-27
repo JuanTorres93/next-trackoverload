845a4c8d6b25a531fc96b03b3337172010d87b08
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-01-27 18:16:15 +0000

    Refactored test to not give false negatives due to timestamps

--- src/application-layer/use-cases/workouttemplate/UpdateWorkoutTemplate/__tests__/UpdateWorkoutTemplate.usecase.test.ts
+++ src/application-layer/use-cases/workouttemplate/UpdateWorkoutTemplate/__tests__/UpdateWorkoutTemplate.usecase.test.ts
@@ -38,7 +38,7 @@ describe('UpdateWorkoutTemplateUsecase', () => {
       };
 
       // Wait just a bit to ensure updatedAt will be different
-      await new Promise((resolve) => setTimeout(resolve, 30));
+      await new Promise((resolve) => setTimeout(resolve, 40));
       const result = await usecase.execute(request);
 
       expect(result.name).toBe('New Name');
