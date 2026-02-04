de8e6b59cf193b7122f6e51c9f1d496919c2d3e5
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-02-04 17:10:24 +0000

    Removed unused GetIngredientsByFuzzyName use case

--- src/interface-adapters/app/use-cases/ingredient/GetIngredientsByFuzzyName/getIngredientsByFuzzyName.ts
+++
@@ -1,5 +0,0 @@
-import { AppIngredientsRepo } from '@/interface-adapters/app/repos/AppIngredientsRepo';
-import { GetIngredientsByFuzzyNameUsecase } from '@/application-layer/use-cases/ingredient/GetIngredientsByFuzzyName/GetIngredientsByFuzzyName.usecase';
-
-export const AppGetIngredientsByFuzzyNameUsecase =
-  new GetIngredientsByFuzzyNameUsecase(AppIngredientsRepo);
