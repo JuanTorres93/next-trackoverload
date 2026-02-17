6e2f1df615f9ccaaa8ccd3b4ec4714260f2d026d
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-02-17 16:03:42 +0000

    Use AppGetMultipleAssembledDaysByIds in server action

--- src/app/_features/day/actions.ts
+++ src/app/_features/day/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { AssembledDayDTO } from '@/application-layer/dtos/DayDTO';
 import { AppGetAssembledDayById } from '@/interface-adapters/app/use-cases/day';
+import { AppGetMultipleAssembledDaysByIds } from '@/interface-adapters/app/use-cases/day';
 import { AppRemoveMealFromDayUsecase } from '@/interface-adapters/app/use-cases/day';
 import { revalidatePath } from 'next/cache';
 
@@ -15,7 +16,7 @@ export async function getAssembledDayById(
   // TODO handle errors
   const assembledDay = await AppGetAssembledDayById.execute({
     dayId,
-    userId: 'dev-user', // TODO: Replace with auth userId
+    userId: 'dev-user', // TODO IMPORTANT: Replace with auth userId
   });
 
   return {
@@ -27,10 +28,21 @@ export async function getAssembledDayById(
 export async function getAssembledDaysByIds(
   dayIds: string[],
 ): Promise<AssembledDayResult[]> {
-  // TODO IMPORTANT: Create a new usecase to fetch a list of assembled days for better performance
-  const assembledDaysPromises = dayIds.map(getAssembledDayById);
+  // TODO handle errors
+  const assembledDays = await AppGetMultipleAssembledDaysByIds.execute({
+    dayIds,
+    userId: 'dev-user', // TODO IMPORTANT: Replace with auth userId
+  });
 
-  return Promise.all(assembledDaysPromises);
+  const assembledDaysMap = new Map<string, AssembledDayDTO>();
+  assembledDays.forEach((day) => assembledDaysMap.set(day.id, day));
+
+  const assembledDaysResults: AssembledDayResult[] = dayIds.map((dayId) => ({
+    dayId,
+    assembledDay: assembledDaysMap.get(dayId) || null,
+  }));
+
+  return assembledDaysResults;
 }
 
 export async function removeMealFromDay(
