b6beadbb76c0383473272555e3047c73f7b0b445
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-02-23 13:38:16 +0000

    Added loading state on ingredient line deletion

--- src/app/_features/ingredient/IngredientLineItem.tsx
+++ src/app/_features/ingredient/IngredientLineItem.tsx
@@ -1,6 +1,7 @@
 'use client';
 import NutritionSummary from '@/app/_ui/NutritionSummary';
 import { IngredientLineDTO } from '@/application-layer/dtos/IngredientLineDTO';
+import { useState } from 'react';
 
 function IngredientLineItem({
   ingredientLine,
@@ -11,11 +12,24 @@ function IngredientLineItem({
   onQuantityChange: (quantity: number) => void;
   onRemove: () => void;
 }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function handleRemove() {
+    setIsLoading(true);
+
+    try {
+      await onRemove();
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <NutritionSummary
       line={ingredientLine}
       onQuantityChange={onQuantityChange}
-      onRemove={onRemove}
+      onRemove={handleRemove}
+      isLoading={isLoading}
     />
   );
 }
