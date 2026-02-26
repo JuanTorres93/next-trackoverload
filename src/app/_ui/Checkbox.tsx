67833811dd3a83d5e0a33bf42324b25cb321756c
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-02-26 10:47:47 +0000

    Styled Checkbox component

--- src/app/_ui/Checkbox.tsx
+++ src/app/_ui/Checkbox.tsx
@@ -1,5 +1,43 @@
-function Checkbox({ ...props }: React.ComponentProps<'input'>) {
-  return <input type="checkbox" {...props} />;
+import React from 'react';
+
+import { HiOutlineCheck } from 'react-icons/hi2';
+
+type CheckboxProps = React.ComponentProps<'input'> & {
+  label?: React.ReactNode;
+};
+
+function Checkbox({ label, ...props }: CheckboxProps) {
+  const { className, ...rest } = props;
+
+  return (
+    <label
+      className={`inline-flex items-center gap-2 cursor-pointer ${className}`}
+    >
+      {/* Real input hidden but accessible */}
+      <input type="checkbox" className="sr-only peer" {...rest} />
+
+      {/* Visual box */}
+      <span
+        className={`
+          w-5 h-5
+          rounded-md
+          border-2
+          border-border
+          text-text-light 
+          flex items-center justify-center
+          transition-all
+          
+          peer-checked:bg-primary-light
+          peer-checked:border-primary-light
+          peer-checked:[&>svg]:opacity-100
+        `}
+      >
+        <HiOutlineCheck size={24} strokeWidth={3} className="opacity-0" />
+      </span>
+
+      {label && <span className="text-sm">{label}</span>}
+    </label>
+  );
 }
 
 export default Checkbox;
