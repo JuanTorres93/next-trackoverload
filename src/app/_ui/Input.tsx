59102da817d7760be9f719663f19e2c14c5c01cd
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-02-26 17:19:02 +0000

    Added disabled styling to Input component

--- src/app/_ui/Input.tsx
+++ src/app/_ui/Input.tsx
@@ -8,12 +8,17 @@ function Input({
   containerClassName?: string;
   children?: React.ReactNode;
 } & React.InputHTMLAttributes<HTMLInputElement>) {
-  const { className, ...rest } = props;
+  const { className, disabled, ...rest } = props;
+
   return (
     <TextRegular
-      className={`flex items-center justify-start border border-border py-1 px-4 rounded-lg outline-none ${containerClassName}`}
+      className={`flex items-center justify-start border border-border py-1 px-4 rounded-lg outline-none ${disabled && 'bg-surface-dark/10 text-text-minor-emphasis border-border/30'} ${containerClassName}`}
     >
-      <input className={`outline-none w-full ${className}`} {...rest} />
+      <input
+        className={`outline-none w-full ${className}`}
+        disabled={disabled}
+        {...rest}
+      />
       {children}
     </TextRegular>
   );
