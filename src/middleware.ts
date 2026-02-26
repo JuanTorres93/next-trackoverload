bc96c98970fa8c57ba2d6e26b02dc25469a76ab0
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-02-26 08:50:01 +0000

    Prevent logged in users to visit login and auth pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,22 @@ import { AppAuthService } from './interface-adapters/app/services/AppAuthService
 import { AuthError } from './domain/common/errors';
 import { cookieSessionName } from './app/api/auth/cookie';
 
+const notAllowedForLoggedInUsers = ['/auth/login', '/auth/register'];
+
 export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
   const token = request.cookies.get(cookieSessionName)?.value;
+
+  if (notAllowedForLoggedInUsers.includes(pathname)) {
+    if (!token) return NextResponse.next();
+
+    const validToken = await AppAuthService.validateToken(token);
+
+    if (validToken) return NextResponse.redirect(new URL('/app', request.url));
+
+    return NextResponse.next();
+  }
 
   if (!token) {
     return NextResponse.redirect(new URL('/auth/login', request.url));
@@ -23,5 +37,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/app/:path*'],
+  matcher: ['/app/:path*', '/auth/login', '/auth/register'],
 };
