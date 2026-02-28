af3275150fac1c55690b079b7864396e5c2b7d21
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-02-28 15:59:23 +0000

    Use JSEND format in addMeal route

--- src/app/api/day/addMeal/route.ts
+++ src/app/api/day/addMeal/route.ts
@@ -1,9 +1,13 @@
+// TODO IMPORTANT: Ensure this module is not used and delete it
+import { JSENDResponse } from '@/app/_types/JSEND';
 import { AppAddMealToDayUsecase } from '@/interface-adapters/app/use-cases/day';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(_req: NextRequest) {
+export async function POST(
+  request: NextRequest,
+): Promise<NextResponse<JSENDResponse<{ message: string }>>> {
   try {
-    const body = await _req.json();
+    const body = await request.json();
     const { dayId, userId, recipeId } = body;
 
     await AppAddMealToDayUsecase.execute({
@@ -12,14 +16,19 @@ export async function POST(_req: NextRequest) {
       recipeId,
     });
 
-    return Response.json(
-      { message: 'Meal added successfully' },
+    return NextResponse.json(
+      {
+        status: 'success',
+        data: {
+          message: 'Meal added to day successfully',
+        },
+      },
       { status: 201 },
     );
   } catch (error) {
     console.log('app/api/day/addMeal: Error adding meal to day:', error);
-    return Response.json(
-      { error: 'Failed to add meal to day' },
+    return NextResponse.json(
+      { status: 'error', message: 'Failed to add meal to day' },
       { status: 500 },
     );
   }
