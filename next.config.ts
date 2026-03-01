54380d126a0fe65c39789831e7736b97c2989bac
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-03-01 19:35:30 +0000

    Add support for openfoodfacts.net image host in production configuration

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const nextConfig: NextConfig = {
           [
             { protocol: 'https', hostname: 'res.cloudinary.com' },
             { protocol: 'https', hostname: 'images.openfoodfacts.org' },
+            { protocol: 'https', hostname: 'images.openfoodfacts.net' },
           ],
   },
 };
