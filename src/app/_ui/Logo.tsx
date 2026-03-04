7788640b843a847f5e31e1d3f644c61b030c91fc
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-03-04 08:43:32 +0000

    Vectorized logo

--- src/app/_ui/Logo.tsx
+++ src/app/_ui/Logo.tsx
@@ -1,11 +1,8 @@
-import logo from '@/../public/logo.png';
 import Image from 'next/image';
 
 function Logo({ size = 50 }: { size?: number }) {
   return (
-    <div>
-      <Image src={logo} width={size} height={size} alt="Logo" />
-    </div>
+    <Image src="/logo.svg" width={size} height={size} alt="Logo" priority />
   );
 }
 
