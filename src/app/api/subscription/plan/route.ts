f487f9bab8be39e609c6fe18348af3f18d57d53f
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-15 09:25:54 +0000

    feat: expose GetPlanInfo use case through an endpoint

---
+++ src/app/api/subscription/plan/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+import { JSENDResponse } from "@/app/_types/JSEND";
+import { PlanInfo } from "@/domain/services/PaymentsService.port";
+import { AppGetPlanInfoUsecase } from "@/interface-adapters/app/use-cases/subscription";
+
+export async function GET(): Promise<NextResponse<JSENDResponse<PlanInfo>>> {
+  try {
+    const planInfo = await AppGetPlanInfoUsecase.execute();
+
+    return NextResponse.json(
+      { status: "success", data: planInfo },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log("api/subscription/plan: Error getting plan info:", error);
+
+    return NextResponse.json(
+      { status: "fail", data: { message: "Failed to get plan info" } },
+      { status: 500 },
+    );
+  }
+}
