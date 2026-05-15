cb7bdd58cb7dfb52dfbe9a990fe370522cd44ab8
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-15 08:41:20 +0000

    feat: expose remove ingredient from recipe use case through an endpoint

---
+++ src/app/api/recipe/[recipeId]/ingredient/[ingredientId]/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+
+import { JSENDResponse } from "@/app/_types/JSEND";
+import { ensureLoggedInUser } from "@/app/api/_common/ensureLoggedInUser";
+import { RecipeDTO } from "@/application-layer/dtos/RecipeDTO";
+import { AppRemoveIngredientFromRecipeUsecase } from "@/interface-adapters/app/use-cases/recipe";
+
+import { handleKnownErrors } from "../../../../_common/handleKnownErrors";
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: Promise<{ recipeId: string; ingredientId: string }> },
+): Promise<NextResponse<JSENDResponse<RecipeDTO>>> {
+  const { recipeId, ingredientId } = await params;
+
+  try {
+    const { currentUserId, notLoggedInResponse } = await ensureLoggedInUser();
+    if (notLoggedInResponse) return notLoggedInResponse;
+
+    const recipe: RecipeDTO =
+      await AppRemoveIngredientFromRecipeUsecase.execute({
+        recipeId,
+        ingredientId,
+        userId: currentUserId,
+      });
+
+    const responseData: JSENDResponse<RecipeDTO> = {
+      status: "success",
+      data: recipe,
+    };
+
+    return NextResponse.json(responseData);
+  } catch (error) {
+    return handleKnownErrors(error as Error);
+  }
+}
