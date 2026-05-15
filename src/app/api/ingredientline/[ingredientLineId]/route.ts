b9047719037d848ad4558a6b049f1ad08fa7b25e
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-15 08:56:49 +0000

    feat: expose UpdateIngredientLine use case through an endpoint

---
+++ src/app/api/ingredientline/[ingredientLineId]/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { JSENDResponse } from "@/app/_types/JSEND";
+import { ensureLoggedInUser } from "@/app/api/_common/ensureLoggedInUser";
+import { IngredientLineDTO } from "@/application-layer/dtos/IngredientLineDTO";
+import { UpdateIngredientLineUsecaseRequest } from "@/application-layer/use-cases/ingredientline/UpdateIngredientLine/UpdateIngredientLine.usecase";
+import { AppUpdateIngredientLineUsecase } from "@/interface-adapters/app/use-cases/ingredientline";
+
+import { handleKnownErrors } from "../../_common/handleKnownErrors";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ ingredientLineId: string }> },
+): Promise<NextResponse<JSENDResponse<IngredientLineDTO>>> {
+  const { ingredientLineId } = await params;
+
+  try {
+    const { currentUserId, notLoggedInResponse } = await ensureLoggedInUser();
+    if (notLoggedInResponse) return notLoggedInResponse;
+
+    const body: UpdateIngredientLineBody = await req.json();
+
+    const ingredientLine = await AppUpdateIngredientLineUsecase.execute({
+      ...body,
+      ingredientLineId,
+      userId: currentUserId,
+    });
+
+    const responseData: JSENDResponse<IngredientLineDTO> = {
+      status: "success",
+      data: ingredientLine,
+    };
+
+    return NextResponse.json(responseData);
+  } catch (error) {
+    return handleKnownErrors(error as Error);
+  }
+}
+
+type UpdateIngredientLineBody = Omit<
+  UpdateIngredientLineUsecaseRequest,
+  "ingredientLineId" | "userId"
+>;
