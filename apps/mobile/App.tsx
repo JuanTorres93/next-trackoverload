05662662db1c51ad20bacd0a4734411ea79700e1
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-16 15:13:28 +0200

    chore: configure typescript and eslint on expo app

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start WORKING ON YOUR APP! NOW!!</Text>
+      <Text>Open up App.js to start WORKING APP! NOW!!</Text>
       <StatusBar style="auto" />
     </View>
   );
