fac3ce389e022948fdc62c383cf0e5f841306e5a
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-27 14:43:27 +0200

    feat: minimal implementation of FinalCTA section - redesign landing page

--- apps/web-and-backend/src/app/(top-navbar)/transfer-to-landing/page.tsx
+++ apps/web-and-backend/src/app/(top-navbar)/transfer-to-landing/page.tsx
@@ -1,5 +1,6 @@
 import CEOMessage from "@/app/_features/marketing/landingV2/CEOMessage/CEOMessage";
 import Features from "@/app/_features/marketing/landingV2/Features/Features";
+import FinalCTA from "@/app/_features/marketing/landingV2/FinalCTA/FinalCTA";
 import Hero from "@/app/_features/marketing/landingV2/Hero/Hero";
 import HowItWorks from "@/app/_features/marketing/landingV2/HowItWorks/HowItWorks";
 import NavBar from "@/app/_features/marketing/landingV2/NavBar";
@@ -19,6 +20,7 @@ export default function LandingPage() {
         <Features />
         <CEOMessage />
         <Pricing />
+        <FinalCTA />
       </div>
     </main>
   );
---
+++ apps/web-and-backend/src/app/_features/marketing/landingV2/FinalCTA/FinalCTA.tsx
@@ -0,0 +1,43 @@
+import { twMerge } from "tailwind-merge";
+
+import ButtonCTA, { ButtonCTASecondary } from "../../landing/ButtonCTA";
+
+function FinalCTA({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  const { className, ...rest } = props;
+
+  return (
+    <section
+      className={twMerge(
+        "h-115 relative rounded-3xl overflow-hidden bg-text flex items-center justify-center ",
+        className,
+      )}
+      {...rest}
+    >
+      <div className="flex flex-col gap-6 text-center text-white z-3 max-w-180">
+        <h2 className="flex flex-col gap-2 text-4xl font-semibold">
+          <span>Start your journey with nutrition.</span>
+
+          <span>Build a strong foundation.</span>
+        </h2>
+
+        <p className="text-[#aeaead]">
+          No more overthinking or waiting for the perfect moment. Take the first
+          step today with Cimientos and create something meaningful for
+          yourself.
+        </p>
+
+        <div className="flex items-center justify-center gap-4 mt-4">
+          <ButtonCTA>Start Your Journey</ButtonCTA>
+
+          <ButtonCTASecondary href="/#features">
+            Explore Features
+          </ButtonCTASecondary>
+        </div>
+      </div>
+
+      <div className="z-2 absolute inset-0 bg-[radial-gradient(circle,var(--color-primary-shade)_0%,transparent_70%)] " />
+    </section>
+  );
+}
+
+export default FinalCTA;
