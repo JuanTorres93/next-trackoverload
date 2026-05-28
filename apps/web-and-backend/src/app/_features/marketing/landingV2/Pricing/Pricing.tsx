b8bb0e5f2c6d7994eb3b64a3c40d5fd2441c918c
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-28 10:32:17 +0200

    feat: implement pricing retrieval and error handling in Pricing component

--- apps/web-and-backend/src/app/_features/marketing/landingV2/Pricing/Pricing.tsx
+++ apps/web-and-backend/src/app/_features/marketing/landingV2/Pricing/Pricing.tsx
@@ -1,5 +1,6 @@
 import { twMerge } from "tailwind-merge";
 
+import { getPlanInfo } from "@/app/_features/subscription/actions";
 import { FREE_TRIAL_DAYS } from "@/domain/common/constants";
 
 import LandingSection from "../LandingSection";
@@ -7,8 +8,58 @@ import PriceCard, { PriceItemType } from "./PriceCard";
 import PriceTimeframeSwitch from "./PriceTimeframeSwitch";
 
 // TODO IMPORTANT: Finish styling when design is done
-function Pricing({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+async function Pricing({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
   const { className, ...rest } = props;
+
+  const planInfoJsend = await getPlanInfo();
+
+  const errorInPriceRetrieval = planInfoJsend.status !== "success";
+
+  if (errorInPriceRetrieval) {
+    return (
+      <LandingSection
+        className={twMerge("", className)}
+        title="Start with nutrition. Build the foundation."
+        {...rest}
+      >
+        <div className="p-4 mb-6 text-red-800 bg-red-100 rounded">
+          Hubo un error al recuperar la información de precios. Por favor,
+          inténtalo de nuevo.
+        </div>
+
+        <div className="flex flex-col items-center gap-6">
+          <PriceTimeframeSwitch />
+
+          <div className="grid grid-cols-2 gap-6">
+            {priceItems.map((item, index) => (
+              <PriceCard key={index} price={item} />
+            ))}
+          </div>
+        </div>
+      </LandingSection>
+    );
+  }
+
+  const flagshipPrice = {
+    priceInEurosCents: planInfoJsend.data.priceInEurCents,
+    tagline: "Pro",
+    shortDescription: `The complete foundation system`,
+    features: [
+      // TODO IMPORTANT: remove this item, since it won't never be true
+      "Full macro tracking (carbs, fat, fiber).",
+      "Advanced progress dashboard.",
+      "Custom food & meals templates.",
+      "Weekly insights & recommendations.",
+      "Priority support.",
+      // TODO IMPORTANT: update when exercise module is released
+      "Exercise tracking (when released).",
+    ],
+    ctaText: "Start Building Your Foundation",
+    isFlagship: true,
+  };
+
+  if (!priceItems.some((item) => item.tagline === flagshipPrice.tagline))
+    priceItems.push(flagshipPrice);
 
   return (
     <LandingSection
@@ -42,23 +93,6 @@ const priceItems: PriceItemType[] = [
       "14-day progress overview.",
     ],
     ctaText: "Get Started Free",
-  },
-  {
-    priceInEurosCents: 500,
-    tagline: "Pro",
-    shortDescription: `The complete foundation system`,
-    features: [
-      // TODO IMPORTANT: remove this item, since it won't never be true
-      "Full macro tracking (carbs, fat, fiber).",
-      "Advanced progress dashboard.",
-      "Custom food & meals templates.",
-      "Weekly insights & recommendations.",
-      "Priority support.",
-      // TODO IMPORTANT: update when exercise module is released
-      "Exercise tracking (when released).",
-    ],
-    ctaText: "Start Building Your Foundation",
-    isFlagship: true,
   },
 ];
 
