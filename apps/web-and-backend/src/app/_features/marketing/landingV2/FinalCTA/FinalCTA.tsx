fab5b93874998268a2341df252e280ab385ca1f3
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-05-28 11:12:50 +0200

    refactor: improving styling in FinalCTA section

--- apps/web-and-backend/src/app/_features/marketing/landingV2/FinalCTA/FinalCTA.tsx
+++ apps/web-and-backend/src/app/_features/marketing/landingV2/FinalCTA/FinalCTA.tsx
@@ -1,12 +1,13 @@
 import { twMerge } from "tailwind-merge";
 
 import ButtonCTA, { ButtonCTASecondary } from "../../landing/ButtonCTA";
+import LandingSection from "../LandingSection";
 
 function FinalCTA({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
   const { className, ...rest } = props;
 
   return (
-    <section
+    <LandingSection
       className={twMerge(
         "h-115 relative rounded-3xl overflow-hidden bg-text flex items-center justify-center ",
         className,
@@ -14,19 +15,19 @@ function FinalCTA({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
       {...rest}
     >
       <div className="flex flex-col gap-6 text-center text-white z-3 max-w-180">
-        <h2 className="flex flex-col gap-2 text-4xl font-semibold">
+        <h2 className="flex flex-col gap-2 text-5xl font-semibold">
           <span>Start your journey with nutrition.</span>
 
           <span>Build a strong foundation.</span>
         </h2>
 
-        <p className="text-[#aeaead]">
+        <p className="text-lg text-[#aeaead]">
           No more overthinking or waiting for the perfect moment. Take the first
           step today with Cimientos and create something meaningful for
           yourself.
         </p>
 
-        <div className="flex items-center justify-center gap-4 mt-4">
+        <div className="flex items-stretch justify-center gap-4 pt-6">
           <ButtonCTA>Start Your Journey</ButtonCTA>
 
           <ButtonCTASecondary href="/#features">
@@ -36,7 +37,7 @@ function FinalCTA({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
       </div>
 
       <div className="z-2 absolute inset-0 bg-[radial-gradient(circle,var(--color-primary-shade)_0%,transparent_70%)] " />
-    </section>
+    </LandingSection>
   );
 }
 
