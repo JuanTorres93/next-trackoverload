4faa4149540387f9c2cc8b68c9b49390c6dceaf9
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-06-06 11:18:35 +0200

    feat: added backdrop blur to navbar

--- apps/web-and-backend/src/app/_ui/Navbar/Navbar.tsx
+++ apps/web-and-backend/src/app/_ui/Navbar/Navbar.tsx
@@ -12,6 +12,23 @@ function Navbar({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={twMerge(
+        "flex flex-col items-center justify-end pb-5 h-36.5 relative",
+        className,
+      )}
+      {...rest}
+    >
+      <NavbarMenu className="z-10" />
+      <NavbarBackdrop className="z-5" />
+    </div>
+  );
+}
+
+function NavbarMenu({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  const { className, ...rest } = props;
+
+  return (
+    <div
+      className={twMerge(
         "p-2.5 bg-secondary-app w-fit rounded-full gap-2.5 flex items-center",
         className,
       )}
@@ -27,6 +44,20 @@ function Navbar({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
       <ButtonExercise />
       <ButtonWeight />
     </div>
+  );
+}
+
+function NavbarBackdrop({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  const { className, ...rest } = props;
+
+  return (
+    <div
+      className={twMerge(
+        "absolute inset-0 bg-linear-to-t from-background-app to-transparent backdrop-blur-2xl mask-[linear-gradient(to_top,black,transparent)]",
+        className,
+      )}
+      {...rest}
+    />
   );
 }
 
