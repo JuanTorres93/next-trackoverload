73b34468676e8b379fa24ac342adaf61cef31847
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-06-07 16:43:14 +0200

    feat: update DaySelector to support date range selection and enhance styling

--- apps/web-and-backend/src/app/_features/meal/redesign/DaySelector.tsx
+++ apps/web-and-backend/src/app/_features/meal/redesign/DaySelector.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import { DayPicker, getDefaultClassNames } from "@daypicker/react";
+import { DateRange, DayPicker, getDefaultClassNames } from "@daypicker/react";
 import "@daypicker/react/style.css";
 import { twMerge } from "tailwind-merge";
 
@@ -10,18 +10,28 @@ function DaySelector({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
   const { className, ...rest } = props;
   const defaultClassNames = getDefaultClassNames();
 
-  const [selected, setSelected] = useState<Date>();
+  const [selected, setSelected] = useState<DateRange>();
+
+  useEffect(() => {
+    console.log("Selected range:", selected);
+  }, [selected]);
 
   return (
     <div className={twMerge("", className)} {...rest}>
       <DayPicker
         animate
-        mode="single"
+        mode="range"
+        ISOWeek
         classNames={{
-          selected: `bg-primary-app border-primary-app rounded-full`,
-          today: `border-secondary-app bg-secondary-app text-white rounded-full`,
-          root: `${defaultClassNames.root} bg-white w-fit p-[15px] rounded-[20px] overflow-hidden`,
+          today: `bg-secondary-app text-white rounded-full`,
+          selected: `bg-primary-light-app rounded-full`,
+          range_start: `bg-primary-light-app rounded-none rounded-l-full`,
+          range_end: `bg-primary-light-app rounded-none rounded-r-full`,
+          range_middle: `bg-primary-light-app rounded-none`,
+
           chevron: `${defaultClassNames.chevron} fill-secondary-app!`,
+
+          root: `${defaultClassNames.root} bg-white w-fit p-[15px] rounded-[20px] overflow-hidden`,
         }}
         selected={selected}
         onSelect={setSelected}
