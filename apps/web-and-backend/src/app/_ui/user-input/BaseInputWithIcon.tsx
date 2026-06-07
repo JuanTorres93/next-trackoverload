13bc5049345bf94e958c7151e9e4cecc0fc10096
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-06-07 19:52:41 +0200

    refactor: separate SearchBar into a reusable InputWithIcon component and SearchBar

---
+++ apps/web-and-backend/src/app/_ui/user-input/BaseInputWithIcon.tsx
@@ -0,0 +1,25 @@
+import { twMerge } from "tailwind-merge";
+
+import Input from "./Input";
+
+function BaseInputWithIcon({
+  icon,
+  ...props
+}: { icon: React.ReactNode } & React.InputHTMLAttributes<HTMLInputElement>) {
+  const { className, ...rest } = props;
+
+  return (
+    <div
+      className={twMerge(
+        "flex gap-2 px-3.75 items-center bg-white rounded-full border border-text-minor-emphasis-app/20 shadow-xs",
+        className,
+      )}
+    >
+      {icon}
+
+      <Input className="w-full px-0 border-none rounded-none" {...rest} />
+    </div>
+  );
+}
+
+export default BaseInputWithIcon;
--- apps/web-and-backend/src/app/_ui/user-input/SearchBar.tsx
+++ apps/web-and-backend/src/app/_ui/user-input/SearchBar.tsx
@@ -1,21 +1,17 @@
 import { HiMiniMagnifyingGlass } from "react-icons/hi2";
 import { twMerge } from "tailwind-merge";
 
-import Input from "./Input";
+import BaseInputWithIcon from "./BaseInputWithIcon";
 
 function SearchBar({ ...props }: React.InputHTMLAttributes<HTMLInputElement>) {
   const { className, ...rest } = props;
 
   return (
-    <div
-      className={twMerge(
-        "flex gap-2 px-3.75 items-center bg-white rounded-full border border-text-minor-emphasis-app/20 shadow-xs",
-        className,
-      )}
-    >
-      <HiMiniMagnifyingGlass className="" size={20} />
-      <Input className="w-full px-0 border-none rounded-none" {...rest} />
-    </div>
+    <BaseInputWithIcon
+      className={twMerge("", className)}
+      icon={<HiMiniMagnifyingGlass className="" size={20} />}
+      {...rest}
+    />
   );
 }
 
