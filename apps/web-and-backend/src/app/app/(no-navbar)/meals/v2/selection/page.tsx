658eec5718cc81da4b23f183ab667580775113d4
Juan Torres <JuanTorresNavarro93@gmail.com>
2026-06-08 15:28:16 +0200

    feat: barebones setup of meal logging page

--- apps/web-and-backend/src/app/app/(no-navbar)/meals/v2/selection/page.tsx
+++ apps/web-and-backend/src/app/app/(no-navbar)/meals/v2/selection/page.tsx
@@ -1,6 +1,9 @@
+import { JSENDResponse, RecipeDTO } from "shared";
+
 import DaySelector, {
   SelectedDays,
 } from "@/app/_features/meal/redesign/DaySelector";
+import { getAllRecipesForLoggedInUser } from "@/app/_features/recipe/actions";
 import Screen from "@/app/_ui/Screen";
 import ButtonAction from "@/app/_ui/buttons/ButtonAction";
 
@@ -14,18 +17,46 @@ export const metadata = {
 };
 
 export default async function SelectMealsPage() {
+  const recipesResponse: JSENDResponse<RecipeDTO[]> =
+    await getAllRecipesForLoggedInUser();
+
+  const hasError = recipesResponse.status !== "success";
+
+  const recipes =
+    recipesResponse.status === "success" ? recipesResponse.data : [];
+
   return (
     <Screen title="Selecciona recetas para registrar" hasBackButton>
       <form
-        className="grid grid-cols-1 grid-rows-[min-content_min-content_min-content_1fr] gap-6.5 h-full"
+        className="grid grid-cols-1 grid-rows-[min-content_min-content_min-content_1fr] gap-3 h-full"
         action=""
       >
         <DateHeader />
 
-        <DaySelector />
-        <SelectedDays />
+        <section>
+          <h2 className="text-[20px] font-semibold">
+            Selecciona las comidas a registrar
+          </h2>
 
-        <ButtonAction className="self-end">Seleccionar comidas</ButtonAction>
+          {hasError && (
+            <span>
+              Hubo un error al cargar tus recetas. Por favor, inténtalo de nuevo
+              más tarde.
+            </span>
+          )}
+
+          {!hasError && (
+            <ul>
+              {recipes.map((recipe) => (
+                <li key={recipe.id} className="flex items-center gap-4">
+                  <span>{recipe.name}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
+
+        <ButtonAction className="self-end">Registrar comidas</ButtonAction>
       </form>
     </Screen>
   );
